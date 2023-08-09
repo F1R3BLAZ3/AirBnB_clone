9331c799417183fcd4bfac2028783f0e8cbdbf87
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-08-09 17:39:31 +0000

    Updates for Task 4

--- models/base_model.py
+++ models/base_model.py
@@ -6,17 +6,28 @@ from datetime import datetime
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
         if args:
             for values in args:
                 print(values)
 
         if kwargs:
+            # Remove '__class__' from kwargs to prevent it from becoming an attribute
+            class_name = kwargs.pop('__class__', None)
+            if class_name:
+                self.__class__ = class_name
+
             for key, value in kwargs.items():
-                setattr(self, key, value)
+                if key == 'created_at' or key == 'updated_at':
+                    # Convert string to datetime
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+        
+        else:
+        # create or assign ID, created_at and updated_at (new instance)
+         self.id = str(uuid4())
+         self.created_at = datetime.now()
+         self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
@@ -30,3 +41,25 @@ class BaseModel:
         instance_dict['created_at'] = self.created_at.isoformat()
         instance_dict['updated_at'] = self.updated_at.isoformat()
         return instance_dict
+
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+print(my_model is my_new_model)
